3121e308cbb3626c9c7098a9d904ffef89a63d49
A21TN013 <103756238+ktgw0919@users.noreply.github.com>
2022-08-24 21:43:04 +0900

    version2.0.0

---
+++ bot.py
@@ -0,0 +1,140 @@
+# 使用するライブラリのインポート
+import discord  #discord.py
+import re       #正規表現
+import random   #ランダム
+import ffmpeg
+import os
+import subprocess
+
+# よくわからん。おまじない。
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# 起動時処理
+@client.event
+async def on_ready():
+    botRoom = client.get_channel(1009335677881696276)   # botが投稿するチャンネルのID取得
+    await botRoom.send("BOTが起動しました!")
+    #サーバーにあるチャンネル情報の取得
+    for channel in client.get_all_channels():
+        print("----------")
+        print("チャンネル名:" + str(channel.name))
+        print("チャンネルID:" + str(channel.id))
+        print("----------")
+    # BOT情報の出力
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):# メッセージが送られた時の処理
+
+    # 送信者がBOTの場合反応しない
+    if message.author.bot:
+        return
+
+    # 挨拶機能
+    if message.content.startswith("おはよう"):  # メッセージが「おはよう」で始まるか調べる
+        m = "おはようございます " + message.author.name + " さん！"  # 返信の内容
+        await message.channel.send(m)# メッセージが送られてきたチャンネルへメッセージを送る
+    
+    
+    # 猫語会話機能
+    # メッセージリスト（以下のどれかを返信）
+    NyanList = [
+        "にゃ～～～～～ん",
+        "にゃ～ん",
+        "にゃ～ん？",
+        "にゃん",
+        "にゃん？"
+        ]
+    # メッセージに"にゃ"が含まれているか調べる
+    pattern=u'にゃ'
+    content = message.content
+    repattern = re.compile(pattern)
+    result=repattern.search(content)
+    if result != None:
+        if client.user != message.author:
+            nyan = NyanList[random.randint(0,4)]    # 返信内容をランダムで決定
+            await message.channel.send(nyan)    # メッセージが送られてきたチャンネルへメッセージを送る
+
+
+    # 読み上げ機能
+    # BOT入退出
+    if message.content == "!join":
+        if message.author.voice is None:
+            await message.channel.send("あなたはボイスチャンネルに接続していません。")
+        # BOTがボイスチャンネルに接続する
+        else:
+            await message.author.voice.channel.connect()
+            await message.channel.send("**" + message.author.voice.channel.name + "** に、*BOT*  が入室しました！")
+    elif message.content == "!leave":
+        if message.guild.voice_client is None:
+            await message.channel.send("BOTはボイスチャンネルに接続していません。")
+        else:
+            # 切断する
+            await message.guild.voice_client.disconnect()
+            await message.channel.send("*BOT* が退出しました！")
+
+
+    # 入力を監視する対象のテキストチャンネル
+    ReadingoutloudCannelIds = [1009332840120451113,1009329150928093224]
+    #メッセージが送られたチャンネルを取得
+    chid=message.channel.id
+    if chid in ReadingoutloudCannelIds:
+        print(0)
+
+
+    #音楽再生
+    #再生処理
+    if message.content == "!play":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。")
+        elif message.guild.voice_client.is_playing():
+            await message.channel.send("再生中です。")
+        else:
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("../million/07 パンとフィルム.m4a"), volume=0.1)
+            message.guild.voice_client.play(source)
+            await message.channel.send("”パンとフィルム”を再生します。")
+
+    #停止処理
+    elif message.content == "!stop":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。")
+
+        # 再生中ではない場合は実行しない
+        elif not message.guild.voice_client.is_playing():
+            await message.channel.send("再生していません。")
+        else:
+            message.guild.voice_client.stop()
+            await message.channel.send("ストップしました。")
+
+
+# チャンネル入退室時の通知処理
+@client.event
+async def on_voice_state_update(member, before, after):
+
+    # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
+    if before.channel != after.channel:
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel(1009335677881696276)
+
+        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+        announceChannelIds = [948454275955183630, 1009119186221539328]
+
+        # 退室通知
+        if before.channel is not None and before.channel.id in announceChannelIds:
+            if member.name != client.user.name:
+                await botRoom.send("**" + before.channel.name + "** から、*" + member.name + "*  が現実に戻りました！")
+        # 入室通知&BOT入室
+        if after.channel is not None and after.channel.id in announceChannelIds:
+            if member.name != client.user.name:
+                await botRoom.send("**" + after.channel.name + "** に、*" + member.name + "*  が現実逃避に来ました！")
+                #await member.voice.channel.connect()
+            
+
+# Botのトークンを指定（デベロッパーサイトで確認可能）
+client.run("hoge")
